94d9790959e75a42d39f10468b7868a9225b33d4
Shahariar Mithu <kmmithu2015@gmail.com>
2024-10-17 16:24:26 +0600

    fixed some issue

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -25,10 +25,13 @@ const TodoItem = ({ todo }) => {
     //console.log(todo.id);
   };
 
+  //console.log(todo.complete);
+
   return (
     <div
-      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black bg-[#ccbed7] w-full
-      ${todo.complete ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`}
+      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+        todo.complete ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+      }`}
     >
       <input
         onChange={handleChecked}
